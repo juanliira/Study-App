756580b1310e5dcd7b867cf34bb381c5910f9296
Juan Victor <102935556+juanliira@users.noreply.github.com>
2024-06-06 17:18:38 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { ThemeProvider } from './src/contexts/ThemeContext'
+
+import MainComponent from './src/components/MainComponent'
+
+const App = () => {
+
+  return (
+    <ThemeProvider>
+      <MainComponent />
+    </ThemeProvider>
+  )
+}
+export default App
